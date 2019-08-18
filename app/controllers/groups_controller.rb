70bcbfd546d2cc2d263463105f03c9844515b8bd
null <punpunsaori@yahoo.co.jp>
2019-08-18 11:15:27 +0900

    create group

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,13 @@
+class GroupsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
---
+++ db/migrate/20190817091816_create_group_users.rb
@@ -0,0 +1,9 @@
+class CreateGroupUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :group_users do |t|
+      t.string :name, null: false
+      t.index :name, unique: true
+      t.timestamps
+    end
+  end
+end
