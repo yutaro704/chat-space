a241cff7ea0fcda3895d9701a88bf642840884bd
null <punpunsaori@yahoo.co.jp>
2019-08-18 15:40:12 +0900

    group function

--- db/migrate/20190817091816_create_group_users.rb
+++ db/migrate/20190817091816_create_group_users.rb
@@ -1,8 +1,8 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :group_users do |t|
-      t.string :name, null: false
-      t.index :name, unique: true
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
