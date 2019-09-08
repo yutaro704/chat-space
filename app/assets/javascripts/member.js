02e7f145603a5bcbbd4c127e63bec0f44b71c88d
null <punpunsaori@yahoo.co.jp>
2019-09-08 13:00:39 +0900

    add chat-member from form

--- app/assets/javascripts/member.js
+++ app/assets/javascripts/member.js
@@ -3,7 +3,7 @@ $(function(){
 function appendUser(user){
   var html = `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="test">追加</a>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
               </div>`
   $(".user-search-result").append(html);
 }
@@ -13,6 +13,19 @@ function appendNoUser(){
                 <p class="chat-group-user__name">一致するユーザーが見つかりません</p>
               </div>`
   $(".user-search-result").append(html);
+}
+
+function addMemberList(name, id){
+  var html = `<div class="chat-group-user clearfix js-chat-member" id="${id}">
+                <input name="group[user_ids][]" type="hidden" value="${id}">
+                  <p class="chat-group-user__name">
+                    ${name}
+                  </p>
+                    <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">
+                      削除
+                    </a>
+              </div>`
+  $(".chat-group-users").append(html);
 }
 
   $("#group_member").on("keyup", function(){
@@ -39,4 +52,12 @@ function appendNoUser(){
       alert('ユーザー検索に失敗しました')
     })
   });
+  $(function(){
+    $(document).on("click", '.user-search-add', function(){
+      var name = $(this).attr("data-user-name")
+      var id = $(this).attr("data-user-id")
+      $(this).parent().remove();
+      addMemberList(name, id);
+    });
+  })
 });
