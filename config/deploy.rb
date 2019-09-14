fa13c7715cdbd862f588ea4c46b55072a16f0378
null <punpunsaori@yahoo.co.jp>
2019-09-14 15:36:13 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,9 @@ lock "~> 3.11.1"
 set :application, "chat-space"
 set :repo_url,  'git@github.com:yutaro704/chat-space.git'
 
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
