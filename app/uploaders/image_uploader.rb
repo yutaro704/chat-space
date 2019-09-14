ebf0099d4643c8e73083207d7b19cf4445a6ffa3
null <punpunsaori@yahoo.co.jp>
2019-09-14 18:26:14 +0900

    fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,6 +7,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
+  # Choose what kind of storage to use for this uploader:
+  storage :fog
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
