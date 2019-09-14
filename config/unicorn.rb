2202e3cc006df9c7937c1e1bc86f45f09dc74d79
null <punpunsaori@yahoo.co.jp>
2019-09-14 14:42:48 +0900

    fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ worker_processes 1
 working_directory "#{app_path}/current"
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path
+#working_directory app_path
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
